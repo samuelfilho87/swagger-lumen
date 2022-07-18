d80e5dddb54da6db33c478d121ef92721950c7c6
Samuel Damasceno Filho <samuelfilho@outlook.com>
2022-07-18 13:40:32 -0300

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    /**
+     * @OA\Info(
+     *   title="TESTE API BLINKET",
+     *   version="1.0",
+     *   @OA\Contact(
+     *     email="support@example.com",
+     *     name="Support Team"
+     *   )
+     * )
+     */
+}
---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/sample/{category}/things",
+     *     operationId="/sample/category/things",
+     *     tags={"yourtag"},
+     *     @OA\Parameter(
+     *         name="category",
+     *         in="path",
+     *         description="The category parameter in path",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Parameter(
+     *         name="criteria",
+     *         in="query",
+     *         description="Some optional other parameter",
+     *         required=false,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Returns some sample category things",
+     *         @OA\JsonContent()
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="Error: Bad request. When required parameters were not supplied.",
+     *     ),
+     * )
+     */
+    public function getThings(Request $request, $category): JsonResponse
+    {
+        $criteria= $request->input("criteria");
+
+        if (! isset($category)) {
+            return response()->json(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        // ...
+
+        return response()->json(["thing1", "thing2"], Response::HTTP_OK);
+    }
+
+    //
+}
